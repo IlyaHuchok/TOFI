a26d394aee080a0a22a41921e3213201a191f1af
IlyaHuchok <Ilya_Huchok@epam.com>
2015-11-29 23:55:30 +0300

    STUFF for testing//

--- BankVictoria/ConsoleTestingApp/Program.cs
+++ BankVictoria/ConsoleTestingApp/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using BankDomainModel;
 
+using BankDAL.Repositories;
+
 using Entities;
 
 namespace ConsoleApplication1
@@ -16,19 +18,51 @@ namespace ConsoleApplication1
         {
             // NOT A PART OF PROJECT
             // ADD ANY CODE FOR TESTING HERE
-          using (var context = new BankDbContext())
-          {
-            //var test = context.RequestStatuses.Where(x => x.Status.Contains("Created")).FirstOrDefault();
-            //context.RequestStatuses.Add(new RequestStatus { Status = "Created" });
-            context.SaveChanges();
-          }
-            //var creditRepository = DEPENCDECY_INJECTION_FRAMEWORK.CREATE<ICreditRepository>();
+            
+            FillDb();
+
             Console.ReadLine();
         }
 
-        //public void Configure()
-        //{
-        //    DEPENCDECY_INJECTION_FRAMEWORK.BINDING_MODULE.Bind<>(CreditRepository).To<ICreditRepository>();
-        //}
+        public static void FillDb()
+        {
+            using (var context = new BankDbContext())
+            {
+
+                //// CLEARS ALL DATA !!!
+                //Console.WriteLine("ALL DATA WILL BE DELETED FROM DB NOW!!! ([ENTER] TO PROCEED)");
+                //Console.ReadLine();
+                //context.Accounts.RemoveRange(context.Accounts);
+                //context.Clients.RemoveRange(context.Clients);
+                //context.Credits.RemoveRange(context.Credits);
+                //context.CreditTypes.RemoveRange(context.CreditTypes);
+                //context.Payments.RemoveRange(context.Payments);
+                //context.Requests.RemoveRange(context.Requests);
+                //context.RequestStatuses.RemoveRange(context.RequestStatuses);
+                //context.Users.RemoveRange(context.Users);
+                //context.SaveChanges();
+                //// CLEARS ALL DATA !!!
+
+                //var statusRepo = new RequestStatusRepository(context);
+                //var statusCreated = new RequestStatus { Status = "Created" };
+                //var statusConfirmed = new RequestStatus { Status = "Confirmed" };
+                //var statusDenied = new RequestStatus { Status = "Denied" };
+                //statusRepo.Add(statusCreated, statusConfirmed, statusDenied);
+
+                //context.SaveChanges();
+                //var confirmedId = statusConfirmed.RequestStatusId;
+
+                //var creditEasy = new CreditType
+                //{
+                //    Name = "Easy Money",
+                //    //Type = "" WTF IS TYPE?????
+                //    Time = TimeSpan.F
+                //}
+
+                //var test = context.RequestStatuses.Where(x => x.Status.Contains("Created")).FirstOrDefault();
+                //context.RequestStatuses.Add(new RequestStatus { Status = "Created" });
+                context.SaveChanges();
+            }
+        }
     }
 }
