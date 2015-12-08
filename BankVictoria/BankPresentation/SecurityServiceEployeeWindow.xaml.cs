46524486200223247b22cac6207eab3c529d6282
kirill <elteriar@mail.ru>
2015-12-08 20:14:48 +0300

    SecurityServiceEmployerWindow version 0.8

---
+++ BankVictoria/BankPresentation/SecurityServiceEployeeWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BankPresentation
+{
+    /// <summary>
+    /// Логика взаимодействия для SecurityServiceEployeeWindow.xaml
+    /// </summary>
+    public partial class SecurityServiceEployeeWindow : Window
+    {
+        public SecurityServiceEployeeWindow()
+        {
+            InitializeComponent();
+            TimeComboBox.Items.Add("All time");
+            TimeComboBox.Items.Add("Year");
+            TimeComboBox.Items.Add("Month");
+            TimeComboBox.Items.Add("Week");
+            TimeComboBox.Items.Add("Day");
+
+            WhereAmountsComboBox.Items.Add(">");
+            WhereAmountsComboBox.Items.Add("<");
+            WhereAmountsComboBox.Items.Add("=");
+        }
+    }
+}
