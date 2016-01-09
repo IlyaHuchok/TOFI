ff7844f196506a04446af09cbd1b6b26a6138207
Ilya Huchok <Ilya_Huchok@epam.com>
2016-01-09 15:39:14 +0300

    missing files added

---
+++ BankVictoria/BankDomainModel/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entities;
+
+namespace BankDomainModel
+{
+    public class DbInitializer : DropCreateDatabaseIfModelChanges<BankDbContext>
+    {
+        protected override void Seed(BankDbContext context)
+        {
+            AddBankAccount(context);
+
+            base.Seed(context);
+        }
+
+
+
+        private void AddBankAccount(BankDbContext context)
+        {
+            var bankAccount = new BankAccount  //Bank Account
+            {
+                Balance = 40 * 1000 * 1000,
+                Currency = "USD"
+            };
+
+            context.BankAccount = bankAccount;
+            context.SaveChanges();
+        }
+    }
+}
