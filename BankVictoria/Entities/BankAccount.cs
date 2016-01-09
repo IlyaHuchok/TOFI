299f12d8e5e48bc1568e99ed75bc9f72472714e6
Ilya Huchok <Ilya_Huchok@epam.com>
2016-01-09 15:34:48 +0300

    bank account

---
+++ BankVictoria/Entities/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class BankAccount
+    {
+        [Key]
+        public int BankAccountId { get; set; }
+        public string Currency { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
